734f943ac76863902de35d2fb355751033929d49
Rendyseftia <rendyseftia0709@gmail.com>
2026-06-09 17:18:49 +0800

    fix media files railway

--- config/urls.py
+++ config/urls.py
@@ -194,12 +194,7 @@ urlpatterns = [
 # MEDIA FILES
 # =========================
 
-if settings.DEBUG:
-
-    urlpatterns += static(
-
-        settings.MEDIA_URL,
-
-        document_root=settings.MEDIA_ROOT
-
-    )
+urlpatterns += static(
+    settings.MEDIA_URL,
+    document_root=settings.MEDIA_ROOT
+)
